6e88f924728d168532a4b607f30047bfefa3243e
zsd7200 <dunhamz2016@gmail.com>
2025-08-08 16:16:13 -0400

    remove hardcoded port

--- components/utils/HeaderHandler.tsx
+++ components/utils/HeaderHandler.tsx
@@ -4,8 +4,9 @@ import { headers } from 'next/headers';
 
 export async function getUrl() {
   const headerList = await headers();
-  const host = (headerList.get('x-forwarded-host') || headerList.get('host'))?.split(':')[0];
-  const port = '25545';
+  const hostArr = (headerList.get('x-forwarded-host') || headerList.get('host'))?.split(':');
+  const host = (hostArr && hostArr.length > 1) ? hostArr[0] : 'localhost';
+  const port = (hostArr && hostArr.length > 1) ? hostArr[1] : '25545';
   const protocol = headerList.get('x-forwarded-proto') || 'http';
   const url = `${protocol}://${host}:${port}`
   return url;
